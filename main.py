05e9442d26ffa6da8408af8ba44872dd192faed0
Miguel Castrejon <miguelcastrejon@mail.weber.edu>
2022-05-21 10:37:03 -0600

    Float Layout

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from kivy.app import App # You always need an App
 from kivy.uix.button import Button # Widget Button
+from kivy.uix.widget import Widget
 
-class FunkyButton(Button):
+class GameScreen(Widget):
     pass
-
 
 class KivyBasicsApp(App): # Make an subclass of App, always. You can call it whatever you want, but preferably end it with App.
     def build(self): # Need build to return a widget all the time. 
-        return FunkyButton(
-            text="FunkyButton",
-            pos=(100,100),
-            size=(500,500),
-            size_hint=(None,None))
-
+        return GameScreen()
 
 if __name__ == "__main__":
     KivyBasicsApp().run() # This is just how you would run it.
